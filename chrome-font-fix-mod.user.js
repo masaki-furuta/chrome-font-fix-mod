a2eadceee55e0c14555738db1e69a6a060fc88b7
Masaki Furuta <masaki.furuta@gmail.com>
2019-03-15 11:46:57 +0900

    Create chrome-font-fix-mod.user.js

---
+++ chrome-font-fix-mod.user.js
@@ -0,0 +1,6 @@
+html, body {
+    -webkit-font-smoothing: subpixel-antialiased !important;
+    -webkit-text-stroke-width: 0.01px !important;
+    text-shadow: 0px 0px 1px rgba(0,0,0,0.1) !important;
+    text-rendering: optimizeLegibility !important;
+}
